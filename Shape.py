693bfba4f7c0c668518fa50bda8a53e0daaeb315
Alex <85442465+elikman1985@users.noreply.github.com>
2022-01-20 13:28:00 +0200

    Hometask_10

---
+++ Shape.py
@@ -0,0 +1,31 @@
+class Shape:
+    def __init__(self, dx, dy):
+        self.dx = dx
+        self.dy = dy
+
+
+class Point(Shape):
+    p_dx = 0
+    p_dy = 0
+
+
+class Circle(Shape):
+    def __init__(self, dx, dy, r):
+        Shape.__init__(self, dx, dy)
+        # super().__init__(dx, dy)
+        self.r = r
+
+    def contains(self, pt):
+        if self.r ** 2 >= (pt.dx - c.dx) ** 2 + (pt.dy - c.dy) ** 2:
+            return True
+        else:
+            return False
+
+    def __contains__(self, pt):
+        return self.r ** 2 >= (pt.dx - c.dx) ** 2 + (pt.dy - c.dy) ** 2
+
+
+p = Point(4, 0)
+c = Circle(1, 0, 3)
+print(c.contains(p))
+print(p in c)
